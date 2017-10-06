8de0ed4c3f18eea1a21b8133db1de71ac2c3247b
Kay <hello@kunalpanchal.in>
2017-10-06 17:33:39 +0530

    Create let_var_const.js

---
+++ let_var_const.js
@@ -0,0 +1,9 @@
+// This prints 10 ten times
+for (var i = 0; i < 10; i++) {
+  setTimeout(function () { console.log(i) }, 0)
+}
+
+// This prints from 1 to 10
+for (let i = 0; i < 10; i++) {
+  setTimeout(function () { console.log(i) }, 0)
+}
