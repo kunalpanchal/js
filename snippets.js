a6c67ad51716e7176b6fdbb0df8d18c890d857f8
Kay <hello@kunalpanchal.in>
2018-09-06 14:58:14 +0530

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -1,3 +1,7 @@
 // Filter duplicates from an array
 
 ['a','b','a'].filter((val, i, arr) => arr.indexOf(val) == i);   // ['a','b']
+
+// Filter duplicates from an array of objects based on its properties
+[{a:1,b:2,c:3},{a:1,b:2,c:4},{a:1,b:4,c:3}].filter((ele, i, self) => i === self.findIndex(t => (t.a === ele.a && t.b === ele.b)))
+// Expected output [{a:1,b:2,c:3},{a:1,b:4,c:3}]
