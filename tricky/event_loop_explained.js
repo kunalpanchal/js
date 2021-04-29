a4c8bc4d3d734e5e2d20e1b8cc48b9cd37556066
Kay <hello@kunalpanchal.in>
2021-04-29 12:40:44 +0200

    Update event_loop_explained.js

--- tricky/event_loop_explained.js
+++ tricky/event_loop_explained.js
@@ -1,7 +1,11 @@
 /* 
 CASE 1 Explanation
 
-When we run the given loop,we see that hello would be printed 100000 times first and only after that the console within the setTimeout gets printed.Also the fact that if it did take 10 seconds to run the for loop and to print "hello" 100000 times then the first 10 "inside setTimout" would be printed suddenly and after that it would print after 1 sec each.This is due to the fact that after the setTimeOut was completed for first few,they were pushed to the event loop and it waited till the call stack was empty (printing hello 100000 times), and only after that "inside setTimeout started to print".
+When we run the given loop,we see that hello would be printed 100000 times first and only after that the console within the setTimeout 
+gets printed.Also the fact that if it did take 10 seconds to run the for loop and to print "hello" 100000 times then the first 10 "inside setTimout" 
+would be printed suddenly and after that it would print after 1 sec each.This is due to the fact that after the setTimeOut was completed for 
+first few,they were pushed to the event loop and it waited till the call stack was empty (printing hello 100000 times), and only after that 
+"inside setTimeout started to print".
 
 Just run the code in the browser console for clarity.
 */
